80c27b85ba10061c11edfd90566c3c5b680317ad
Sun Min Lee <30864676+sunminkr@users.noreply.github.com>
2022-06-08 14:04:52 +0900

    Update MergeSortedArray.cc

--- MergeSortedArray.cc
+++ MergeSortedArray.cc
@@ -1,9 +1,6 @@
 /* 
     88. Merge Sorted Array
-
-    #1. minimize the size of array given
-    #2. merge
-    #3. sort
+    O(2N)
  */
 
 #include <iostream>
@@ -14,33 +11,27 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        int it = 0;
-        for(int i = 0; i < n;) {
-            if(it == n) {
-                nums1.erase(nums1.begin()+it-1, nums1.end());
+        int itr=0;
+        for (int i=0; i<n;) {
+            if(nums1[itr] == 0) {
+                nums1.erase(nums1.begin()+itr, nums1.end());
                 nums1.insert(nums1.end(), nums2.begin()+i, nums2.begin()+n);
-                cout << "break print vector" << endl;
-                printVector(nums1);
                 break;
             }
-            cout << "for print vector" << endl;
-            printVector(nums1);
-            if(nums1.at(it) >= nums2[i]) {
-                nums1.pop_back();
-                nums1.insert(nums1.begin()+it, nums2[i]);
-                i++;
-                cout << "if print vector" << endl;
-                printVector(nums1);
+            else {
+                if(nums1[itr] >= nums2[i]) {
+                    nums1.pop_back();
+                    nums1.insert(nums1.begin()+itr, nums2[i]);
+                    i++;
+                }
+                itr++;
             }
-            else it++;
         }
     }
     void printVector(vector<int> nums1) {
         for (auto& r : nums1) {
             cout << r << endl;
         }
-        cout << "=============" << endl;
     }
 };
 
@@ -49,11 +40,9 @@ int main() {
     vector<int> nums1 = {4, 0, 0, 0, 0, 0};
     vector<int> nums2 = {1, 2, 3, 5, 6};
 
-    int m = 0;
-    int n = 1;
+    int m = 1;
+    int n = 5;
 
     s.merge(nums1, m, nums2, n);
-    for (auto& r : nums1) {
-        cout << r << endl;
-    }
+    s.printVector(nums1);
 }
