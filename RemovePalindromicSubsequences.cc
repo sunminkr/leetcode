bde4fd5af56e402bdedd3ecf17e29abd9501c2bc
Sun Min Lee <30864676+sunminkr@users.noreply.github.com>
2022-06-09 10:11:55 +0900

    Create RemovePalindromicSubsequences.cc

---
+++ RemovePalindromicSubsequences.cc
@@ -0,0 +1,38 @@
+/* 
+    1332. Remove Palindromic Subsequences
+    O()
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        if(s == "") return 0;
+        else if(isPalindrome(s)) return 1;
+        else return 2;
+    }
+    bool isPalindrome(string s) {
+        int front = 0;
+        int back = s.size()-1;
+
+        for (int i=0; i<(s.size()/2)+1; i++) {
+            if(s.at(front) == s.at(back)) {
+                front++;
+                back--;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution s;
+    string input = "babab";
+    cout << s.removePalindromeSub(input) << endl;
+}
